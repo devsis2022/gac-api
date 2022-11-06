c0b457a02cb51169bf7a9dc3ed38ef573e6cf479
Henrique Farias <henrique.biasibetti@gmail.com>
2022-11-06 00:25:03 -0300

    chore: configurando webpack

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const NodemonPlugin = require('nodemon-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
+// const CopyPlugin = require('copy-webpack-plugin')
+const dotenv = require('dotenv')
+dotenv.config()
+
+module.exports = {
+  context: __dirname,
+  mode: process.env.NODE_ENV || 'development',
+  entry: {
+    main: 'src/server.ts'
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.mjs', '.json', '.ts'],
+    symlinks: false,
+    cacheWithContext: false,
+    plugins: [
+      new TsconfigPathsPlugin({
+        configFile: './tsconfig.json'
+      })
+    ]
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js'
+  },
+  optimization: {
+    concatenateModules: false
+  },
+  target: 'node',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.(tsx?)$/,
+        loader: 'ts-loader',
+        exclude: [
+          [
+            path.resolve(__dirname, 'node_modules'),
+            path.resolve(__dirname, '.serverless'),
+            path.resolve(__dirname, '.webpack'),
+            path.resolve(__dirname, '.docker')
+          ]
+        ],
+        options: {
+          transpileOnly: true,
+          experimentalWatchApi: true
+        }
+      }
+    ]
+  },
+  plugins: [new NodemonPlugin(), new Dotenv()]
+}
