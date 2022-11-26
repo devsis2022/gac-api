c235fe1371225431531f34834d1ab4ba25cd3e26
Henrique Farias <henrique.biasibetti@gmail.com>
2022-11-26 20:36:44 -0300

    fix: bind UserRolesRepository

--- src/config/ioc/repository.module.ts
+++ src/config/ioc/repository.module.ts
@@ -1,8 +1,10 @@
 import { ContainerModule, interfaces } from 'inversify'
 import { UserRepository } from 'src/repositories/user.repository'
 import { InstitutionRepository } from 'src/repositories/institution.repository'
+import { UserRolesRepository } from 'src/repositories/user-roles.repository'
 
 export const repositoryModule = new ContainerModule((bind: interfaces.Bind) => {
   bind(UserRepository).toSelf()
   bind(InstitutionRepository).toSelf()
+  bind(UserRolesRepository).toSelf()
 })
